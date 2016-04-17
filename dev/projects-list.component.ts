b35019e7d7047abd46b74eed59b94926b334c512
igots5cats@yahoo.com <g1ddyUp>
2016-04-17 09:11:54 -0700

    fix ag-grid to work.

--- dev/projects-list.component.ts
+++ dev/projects-list.component.ts
@@ -1,13 +1,19 @@
 import {Component} from 'angular2/core';
 import {AgGridNg2} from 'ag-grid-ng2/main';
 
+
 @Component({
     selector: 'projects-list',
     templateUrl: 'templates/projects-list.tpl.html',
     directives: [AgGridNg2]
 })
 export class ProjectsListComponent {
-    public my_data = [{"id": 1, "name": "Robin"}, {"id": 2, "name": "Greg"}];
+    columnDefs = [
+        { headerName: "ID", field: "id" },
+        { headerName: "Name", field: "name" }];
 
-    public columnDefs = [{headerName: 'id'}, {headerName: 'name'}];
+    rowData = [{id:1, name:"robin"}, {id:2, name:"greg"}];
+
+
 }
+

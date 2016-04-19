07a3791234a5c0bb1528c643b68df1eb8424a534
igots5cats@yahoo.com <g1ddyUp>
2016-04-18 21:43:19 -0700

    data service, save to firebase.

--- dev/add-project.component.ts
+++ dev/add-project.component.ts
@@ -2,29 +2,46 @@
  * Created by rluther on 4/18/2016.
  */
 import {Component} from 'angular2/core';
+import {DataService} from "./data.service";
 
 @Component({
-    selector:'add-project',
-    template:`
+    selector: 'add-project',
+    template: `
 <div>
 <div class="input">
 <label for="name">Name</label>
 <input type="text" id="name" #name>
 </div>
 <div class="input">
-<label for="notes">Name</label>
+<label for="notes">Notes</label>
 <input type="text" id="notes" #notes>
 </div>
 <button (click)="onPost(name.value, notes.value)">Post Data</button>
 <button (click)="onGetPosts()">Get All Posts</button>
+<p>Response: {{response}}</p>
 </div>
-`
+`,
+    providers: [DataService]
 })
 
-export class AddProjectComponent{
-    response: string;
+export class AddProjectComponent {
+    response:string;
 
-    onPost()
+    constructor(private _dataService:DataService){}
 
-    onGetPosts()
+    onPost(name:string, notes: string) {
+        // create javascript object with the data
+        const data = {
+            name: name,
+            notes:notes
+        };
+        this._dataService.postData(data)
+            .subscribe(
+                data => this.response = JSON.stringify(data),
+                error=> console.log(error)
+            ); //get stuff back from observable
+    }
+
+    onGetPosts() {
+    }
 }
--- dev/data.service.ts
+++ dev/data.service.ts
@@ -1,15 +1,23 @@
 import {Injectable} from "angular2/core";
-import {Http} from "angular2/http";
-
+import {Http, Headers} from "angular2/http";
+import 'rxjs/Rx';
 
 @Injectable()
-export class DataService{
-    
-    constructor(private _http: Http){}
+export class DataService {
 
-    postData(data: any){
+    constructor(private _http:Http) {
+    }
 
+    postData(data:any) {
         // need URL, the data
-        
+        const body = JSON.stringify(data); //. javascript object ("data") to json, string
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        //observable
+        // the return will be key info. he calls the http.post an observable.
+        return this._http.post('https://XYZ.firebaseio.com/data.json',
+            body, {headers: headers})
+            .map(response=>response.json()); // map response. data stream to json
+
     }
 }
